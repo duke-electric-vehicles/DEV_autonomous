a31ff0f153a4566fe31048b888ea8fcfa990b3bd
Patrick Grady <hmfpa392@ameritech.net>
2018-11-21 02:42:29 -0500

    justification for why this method exists

--- PathPlanning/basicPathPlanning/util/gradient_gerry.m
+++ PathPlanning/basicPathPlanning/util/gradient_gerry.m
@@ -1,6 +1,10 @@
 function [dA] = gradient_gerry(A)
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
+%gradient_gerry reimplementation of the gradient built-in function
+%   I wanted to try this to see if it would help mitigate the oscillations
+%   but it didn't work.  The reason I thought it might help is because the
+%   matlab gradient function does a center-point style dervative which
+%   means 3 points contribute to the derivative instead of just one which I
+%   thought could potentially cause numerical issues.  Alas, it was not so.
     dA = zeros(size(A));
     if (size(A,2)>1)
         dA(:,1:size(A,2)-1) = diff(A,1,2);
